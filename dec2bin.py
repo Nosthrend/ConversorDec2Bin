6b403cbdf256087b3de6342425e89dd91c99e27d
Joan Castells Appriou <Castells.Joan.30@gmail.com>
2021-09-16 16:10:44 +0200

    Actualizado dec2bin.py
    En los últimos dos cambios se han realizado actualizaciones al código de mencionado archivo para capacitarlo a la traducción a binario de números negativos.

--- dec2bin.py
+++ dec2bin.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------
 # Convierte un número decimal positivo a binario usando un 
-#   determinado número de bits.
+#    determinado número de bits.
 # El binario resultante es un string e.g. "101"
 # Se usa la función bin() que transforma e.g. 3 en "0b11".
 # En esta función se quita el "0b" para dejar el "11"
